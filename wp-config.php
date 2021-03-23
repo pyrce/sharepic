99e03e5c2d36d36a2593bdf41238b52282437616
florent etheve <fetheve2@gmail.com>
2021-03-23 14:25:01 +0400

    db var

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
  * @package WordPress
  */
 if(isset($_ENV[`DATABASE_URL`])) {
-  $db = parse_url($_ENV[`CLEARDB_DATABASE_URL`]);
+  $db = parse_url($_ENV[`DATABASE_URL`]);
   define('DB_NAME', trim($db[`path`],`/`));
   define('DB_USER', $db[`user`]);
   define('DB_PASSWORD', $db[`pass`]);
